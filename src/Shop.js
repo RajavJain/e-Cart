f2fc0770065c8fa121ce245e2c582b78c2b4a87c
RajavJain <rajavjain2002@gmail.com>
2022-11-18 21:39:40 +0530

    Added FrontEnd Part

---
+++ src/Shop.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+        <>
+        <div className='container my-4'>
+            <button className="btn btn-primary mx-2"> - </button>
+                ADD Items TO CART
+            <button className="btn btn-primary mx-2"> + </button>
+        </div>
+        </>
+  )
+}
+
+export default Shop
