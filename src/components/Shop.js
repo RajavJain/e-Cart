458f2327e009cda7c309856957654ac86d4d76e6
RajavJain <rajavjain2002@gmail.com>
2022-11-19 01:19:49 +0530

    Used bindActionCreators

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-// import { bindActionCreators } from 'redux';
+import { bindActionCreators } from 'redux';
 import {actionCreator} from '../state/index';
-// export * as actionCreator from "../state/action-creator/index" 
+
 
 
 const Shop = () => {
   const dispatch= useDispatch();
+  // Used the help of destructuring
+  const {delItem,addItem}= bindActionCreators(actionCreator,dispatch)
 
   return (
         <>
         <div className='container my-4'>
-            <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreator.delItem(1))}}> - </button>
+            <button className="btn btn-primary mx-2" onClick={()=>{delItem(1)}}> - </button>
                 ADD Items TO CART
-            <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreator.addItem(1))}}> + </button>
+            <button className="btn btn-primary mx-2" onClick={()=>{addItem(1)}}> + </button>
+
+            {/* OR WITHOUT BINDACTIONCREATOR  */}
+            {/* <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreator.delItem(1))}}> - </button>
+                ADD Items TO CART
+            <button className="btn btn-primary mx-2" onClick={()=>{dispatch(actionCreator.addItem(1))}}> + </button> */}
         </div>
         </>
   )
